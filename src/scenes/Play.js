0752d6b53a82cf5dd0b97ed0c8c7bc54071982da
Frank Shi <frshi@ucsc.edu>
2023-10-12 00:04:51 -0700

    finished sfx and menu

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -61,16 +61,37 @@ class Play extends Phaser.Scene {
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
+
+        // GAME OVER flag
+        this.gameOver = false;
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
 
     update(){
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.start('menuScene');
+        }
+
         this.startfield.tilePositionX -=4;
-        this.p1Rocket.update();
 
-        this.ship01.update();               // update spaceships (x3)
-        this.ship02.update();
-        this.ship03.update();
-
+        if(!this.gameOver){
+            this.p1Rocket.update();             // update rocket sprite
+            this.ship01.update();               // update spaceships (x3)
+            this.ship02.update();
+            this.ship03.update();
+        }
+        
         //check collisions
         if (this.checkCollision(this.p1Rocket, this.ship03)){
             this.p1Rocket.reset();
@@ -112,5 +133,7 @@ class Play extends Phaser.Scene {
         // score add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+        
+        this.sound.play('sfx_explosion');
     }
 }
